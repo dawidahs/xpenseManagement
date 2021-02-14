5cec5edd37fc674ce060c8a7791caca6d43d3f75
DavidHS <dawidhdez@gmail.com>
2021-02-14 13:08:56 +0100

    Added delete button

--- urls.py
+++ urls.py
@@ -3,6 +3,10 @@ from django.urls import path
 from my_expenses import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import url
+
+
+app_name = 'exp'
 
 urlpatterns = [
     path('', views.home),
@@ -10,8 +14,8 @@ urlpatterns = [
     #path('upload/', views.expense_form),
     path('index/', views.expenses_list, name="List"),
     path('index/expense_form/', views.expense_form, name="Expense Entry"),
+    url(r'^delete/(?P<pk>[0-9]+)/$', views.expense_delete, name='expense_delete')
 ]
 
 #if settings.DEBUG:
     #urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
--- views.py
+++ views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.core.files.storage import FileSystemStorage
 from .models import Expense
 from .forms import expenseEntry
+from django.contrib import messages
 
 # Create your views here.
 
@@ -22,16 +23,20 @@ def expenses_list(request):
     all_expenses = Expense.objects.all()
     return render(request, 'my_expenses/index.html', {'all_expenses': all_expenses})
 
-def expenses_edit(request, pk):
-    all_expenses = Expense.objects.all()
-    return render(request, 'my_expenses/index.html', {'all_expenses': all_expenses})
-    
+def expense_delete(request, pk):
+    expense = get_object_or_404(Expense, pk=pk)
+    if request.method == "POST":
+        expense.delete()
+        return redirect("../../index/")
+    return render(request, 'my_expenses/index.html', {'expense': expense})
+
 def expense_form(request):
     if request.method == 'POST':
         form = expenseEntry(request.POST, request.FILES)
         if form.is_valid():
             form.save()
             img_obj = form.instance
+            messages.success(request, 'Form submission successful')
             return render(request, 'my_expenses/index.html', {'form': form, 'img_obj': img_obj})
     else:
         form = expenseEntry()
